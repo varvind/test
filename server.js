21e83d80e8e575e3bdcdcb4743a23bebd2e6b484
Arvind Venkatesan <varvind@tamu.edu>
2019-12-26 23:07:51 -0600

    all the stuff

---
+++ server.js
@@ -0,0 +1,38 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const User = require('../Club Archive Tester/models/users')
+
+
+
+app.set('view engine', 'ejs')
+app.set ('views', __dirname + '/views')
+
+
+app.use(express.static('public'))
+app.use(express.static('views'))
+app.use(bodyParser.urlencoded({limit:'10mb', extended : false}))
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
+
+app.post("/adduser",  async (req, res) => {
+    var data = new User(req.body)
+    try{
+        const newUser = await data.save()
+        res.redirect('../views/index.html')
+    }catch {
+        console.log("Cannot save to database")
+    }
+})
+
+app.listen(process.env.PORT || 3000)
