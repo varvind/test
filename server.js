2a7c3ec3619cfd6d58d86eb0a8338dea7b10e289
Arvind Venkatesan <varvind@tamu.edu>
2019-12-28 11:59:05 -0600

    sign up to local database fixed and working

--- server.js
+++ server.js
@@ -20,19 +20,22 @@ app.use(express.static('public'))
 app.use(express.static('views'))
 app.use(bodyParser.urlencoded({limit:'10mb', extended : false}))
 
+mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE_URL)
+
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
 
-app.post("/adduser",  async (req, res) => {
-    var data = new User(req.body)
-    try{
-        const newUser = await data.save()
-        res.redirect('./views/index.html')
-    }catch {
-        console.log("Cannot save to database")
-    }
+app.post("/adduser",(req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.sendFile(__dirname + "/views/index.html");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
 })
 
 app.listen(process.env.PORT || 3000)
