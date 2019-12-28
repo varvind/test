a9130641e5c7a03c1154a56465fe28d63ff6f7d8
Arvind Venkatesan <varvind@tamu.edu>
2019-12-27 18:55:19 -0600

    correct route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const User = require('../test/models/users')
+const User = require('./models/users')
 
 
 
